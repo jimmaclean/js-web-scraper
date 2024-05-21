65ee1333721fb7fca7c1e2bd18df46b80f787cd0
Jim Maclean <jqim87@gmail.com>
2024-05-21 07:12:13 +0100

    remove extra async

--- brands.js
+++ brands.js
@@ -2,18 +2,14 @@ const fs = require("fs").promises;
 const cheerio = require("cheerio");
 
 (async () => {
-  const html = await fs.readFile("brands.html");
-  const data = await readLabels(html);
-  fs.writeFile("brands.json", JSON.stringify(data, null, 2), () => {});
-})();
-
-async function readLabels(html) {
   const data = [];
+  const html = await fs.readFile("brands.html");
   const $ = cheerio.load(html);
+
   $("label").each(function () {
     const brand = $(this);
     data.push(brand.text().trim());
   });
-  console.log(data);
-  return Promise.resolve(data);
-}
+
+  fs.writeFile("brands.json", JSON.stringify(data, null, 2), () => {});
+})();
